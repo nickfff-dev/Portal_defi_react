ed6001eff4aa7af74546806c45ae74dc9b9ce21c
nickfff-dev <hunchographics@gmail.com>
2022-02-17 23:09:50 +0300

    first commit

--- src/Presstwo.js
+++ src/Presstwo.js
@@ -9,19 +9,19 @@ class Presstwo extends Component {
 
     <div className="container">
       <div className="iframely-embed">
-        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" src="//cdn.iframe.ly/vuE67Zm?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
+        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" title = "iframe4" src="//cdn.iframe.ly/vuE67Zm?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
       </div><br/>
       <div className="iframely-embed">
-        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" src="//cdn.iframe.ly/mF5t7cQ?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
+        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" title = "iframe3" src="//cdn.iframe.ly/mF5t7cQ?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
       </div><br/>
       <div className="iframely-embed">
-        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" src="//cdn.iframe.ly/36BKLO0?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
+        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" title = "iframe2" src="//cdn.iframe.ly/36BKLO0?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
       </div><br/>
       <div className="iframely-embed">
-        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" src="//cdn.iframe.ly/MvSt0vT?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
+        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" title = "iframe1" src="//cdn.iframe.ly/MvSt0vT?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
       </div><br/>
       <div className="iframely-embed">
-        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" src="//cdn.iframe.ly/HIjmHpW?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
+        <div className="iframely-responsive" style={{height: "162px", paddingBottom: "0px"}}><iframe allowFullScreen="" allow="autoplay *; encrypted-media *" title = "iframe0" src="//cdn.iframe.ly/HIjmHpW?card=small&amp;v=1&amp;app=1" style={{border: "1px solid rgb(222, 222, 222)", boxShadow: "rgba(0, 0, 0, 0.06) 0px 1px 3px"}}></iframe></div>
       </div>
     </div>
 
