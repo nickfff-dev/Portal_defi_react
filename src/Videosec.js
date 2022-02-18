96993e844bb303d05701a0fdc0ef27a1a4e27744
nickfff-dev <hunchographics@gmail.com>
2022-02-18 03:03:45 +0300

    good changes

--- src/Videosec.js
+++ src/Videosec.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import "./videosec.css";
-import { Embed } from 'semantic-ui-react'
 import embeddata from './embedyoutube';
+import { Embed } from 'semantic-ui-react'
 
 class Videosec extends Component {
     constructor(props) { 
@@ -11,7 +11,15 @@ class Videosec extends Component {
         }
     }
 
-    render() {
+
+
+    onClickHandler(e) {
+        e.target.url=`https://www.youtube.com/embed/${e.target.id}/?autoplay=1`
+    }
+  
+
+
+    render() {  
         return (
             <div className="section video-section" id="videos">
             <div className="black-fade"></div> 
@@ -22,9 +30,10 @@ class Videosec extends Component {
         
                 <div className="breweries">
                     <ul>
-                        {this.state.dataforembed.map((embedy, index)=>{
+                        {this.state.dataforembed.map((embedy, index)=>{ 
   return (
-      <li key={index}>  <Embed key={index} id={embedy.id} placeholder= {embedy.placeholder} source='youtube' width="100%" height="200" className= "embed-youtube"/><p key= {index}>{embedy.title}</p> </li>)})}
+      <li key={index}>  <Embed as="div" id={embedy.id} autohide={false} hd={true}  url={`https://www.youtube.com/embed/${embedy.id}`} width="100%" height="500" autoplay={true} onClick={this.onClickHandler.bind(this)} active= "true" className="embed" style={{ background: `url(${embedy.placeholder}) center center / cover rgb(0, 0, 0)`,  width: "339px", height: "150px" }}/> 
+      <p>{embedy.title}</p>  </li>)})}
         
                     </ul>
                 </div>
