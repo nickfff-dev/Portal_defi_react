914f9620df9b10d19f2321aea1e1c4a353382e4a
nickfff-dev <hunchographics@gmail.com>
2022-02-18 22:56:38 +0300

    good

--- src/Contact.js
+++ src/Contact.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import "./contact.css";
 
 class Contact extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            email: '',
+            subject: '',
+            message: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    
+    handleChange = (event) => { 
+        event.preventDefault();
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    
+
+
     render() {
         return (
             <div className="section institutions" id="institutions">
@@ -13,9 +34,9 @@ class Contact extends Component {
         <h1>Contact Us</h1>
                 
         <form id="contact-form">
-            <input className="field" type="text" placeholder="Email address (required)" style={{marginRight: "4px"}} name="EMAIL"/>
-            <input className="field" type="text" placeholder="Subject" name="SUBJECT"/>
-            <textarea placeholder="Message (required)" name="MESSAGE"></textarea>
+            <input className="field" type="text" onChange={this.handleChange} placeholder="Email address (required)" style={{marginRight: "4px"}} value={this.state.name} name="email"/>
+            <input className="field" type="text" onChange={this.handleChange} placeholder="Subject" value={this.state.subject} name="subject"/>
+            <textarea placeholder="Message (required)" onChange={this.handleChange} value={this.state.name} name="message"></textarea>
             <input type="submit" className="submit" value="Contact us"/>
         </form>
     
